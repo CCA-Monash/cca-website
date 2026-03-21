948c79fa3cb3b15bcfe648c3a8510d64e03b7f62
Sener <sset0014@student.monash.edu>
2026-03-21 18:12:43 +1100

    Styling Changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../../assets/logo1.png";
+import logo from "../../assets/logo2.png";
 
 const Navbar: React.FC = () => {
   return (
